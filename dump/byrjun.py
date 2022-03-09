19c08bff613a7ebb6def7af11c82767900902fa2
jason <jasonhelgi05@gmail.com>
2022-03-09 15:55:44 +0000

    comm

---
+++ dump/byrjun.py
@@ -0,0 +1,21 @@
+print("1.")
+print("2.")
+print("3.")
+print("4.")
+print("5.")
+val= int(input("hvaða lið villtu velja"))
+
+if val == 1:
+    print("þú valdir lid 1")
+
+if val == 2:
+    print("þú valdir lið 2")
+
+if val == 3:
+    print("þú valdir lið 3")
+
+if val == 4:
+    print("þú valdir lið 4")
+
+if val == 5:
+    print("þú valdir lið 5")
